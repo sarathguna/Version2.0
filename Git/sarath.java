5404e0bc8c0b6059ea1b6ce3b13131b1a3a2d1e3
sarath <sarathgunalan@gmail.com>
2019-06-17 23:37:26 +0530

    Running Java Files

--- Git/sarath.java
+++ Git/sarath.java
@@ -1,12 +1,19 @@
-hai
+public class sarath
+{
 
-how r u 
+    public static void main(String[] args) {
 
-fine
+        int n = 200, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
 
-what's your name 
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
 
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
 
-i'm sarath
-
-
+}
